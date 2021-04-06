fc8f3d4c142bf1c371f4c8dafe6f07b45acb9866
Aman Jaiman <amanjaiman@outlook.com>
2021-04-05 22:57:53 -0400

    host

--- mysite/settings.py
+++ mysite/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ['secret_key']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['owl-pickem.herokuapp.com']
 
 
 # Application definition
--- mysite/urls.py
+++ mysite/urls.py
@@ -17,11 +17,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pickem/', include('pickem.urls'))
 ]
 
-if settings.DEBUG:
-     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+#if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
