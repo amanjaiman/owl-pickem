97eb9fdec2974e1260529cb87a315b7fe32f09fa
Aman Jaiman <amanjaiman@outlook.com>
2021-04-06 00:21:27 -0400

    Change url path

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,7 +20,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('pickem/', include('pickem.urls'))
+    path('', include('pickem.urls'))
 ]
 
 #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
